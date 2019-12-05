145234467754dab237d883bd6e3dd3597b8f34e7
xuyuankai1995 <xuyuankai1995@foxmail.com>
2019-12-05 22:16:53 +0800

    主干模块
    主干模块

---
+++ src/main/java/com/elderly/aid/AidApplication.java
@@ -0,0 +1,13 @@
+package com.elderly.aid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elderly/aid/AidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elderly.aid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
